14dcc312fa76bafd5d5592e8393d6773fc1cc6e8
Joaquim José <joaquimmatoso01@gmail.com>
2023-03-27 16:12:32 +0100

    Initial commit

---
+++ src/main/java/com/bjdelivery/bjdelivery/BjdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.bjdelivery.bjdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjdelivery/bjdelivery/BjdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjdelivery.bjdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
