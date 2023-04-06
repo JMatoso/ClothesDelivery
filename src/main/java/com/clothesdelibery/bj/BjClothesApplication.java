390ff445f9d9da405eed77bfc91b715ad44178ff
Joaquim José <joaquimmatoso01@gmail.com>
2023-04-06 20:37:48 +0100

    Spring MVC

---
+++ src/main/java/com/clothesdelibery/bj/BjClothesApplication.java
@@ -0,0 +1,13 @@
+package com.clothesdelibery.bj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjClothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clothesdelibery/bj/BjClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothesdelibery.bj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
