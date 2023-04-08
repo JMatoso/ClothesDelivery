7edf78637858e936f14f198655eb4d4be0dfd2ed
Joaquim José <joaquimmatoso01@gmail.com>
2023-04-08 23:46:57 +0100

    Finalmente, alguma coisa.

--- src/main/java/com/clothesdelivery/web/controller/ClothesController.java
+++ src/main/java/com/clothesdelivery/web/controller/ClothesController.java
@@ -1,4 +1,12 @@
 package com.clothesdelivery.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ClothesController {
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
 }
