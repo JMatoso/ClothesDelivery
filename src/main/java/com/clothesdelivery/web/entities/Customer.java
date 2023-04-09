412c771b706b9af03e2867546dec9e3f2c7af9eb
Joaquim José <joaquimmatoso01@gmail.com>
2023-04-09 02:09:02 +0100

    Customer

---
+++ src/main/java/com/clothesdelivery/web/entities/Customer.java
@@ -0,0 +1,32 @@
+package com.clothesdelivery.web.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate birthdate;
+    private String phoneNumber;
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdTime;
+}
