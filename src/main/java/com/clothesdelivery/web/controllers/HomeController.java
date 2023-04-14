72b910d9266cdef6aa3ac077fb491722414a139f
Joaquim José <joaquimmatoso01@gmail.com>
2023-04-14 21:21:37 +0100

    Terms, Contact and About Us - Tudo inventando, muita inspiração ya

---
+++ src/main/java/com/clothesdelivery/web/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.clothesdelivery.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/terms")
+    public String terms() {
+        return "terms";
+    }
+}
