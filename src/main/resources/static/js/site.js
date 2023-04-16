3be76d0b0f0c66ed09d333d2a54bb03357aebfe2
Joaquim José <joaquimmatoso01@gmail.com>
2023-04-17 00:07:58 +0100

    Index Partial

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,7 +1,7 @@
-$('.btn-filter').on('click', function() {
+/*$('.btn-filter').on('click', function() {
     const filter = $(this).data('filter');
     const $items = $('.product-item');
 
     $items.filter(':not(.' + filter + ')').removeClass('show');
     $items.filter('.' + filter).addClass('show');
-});
+});*/
