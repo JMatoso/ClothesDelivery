44dc9d0d683a2c414c16bb9b72e77e88a1c8cf9c
Joaquim José <joaquimmatoso01@gmail.com>
2023-05-20 16:35:53 +0100

    Features

--- src/main/java/com/clothesdelivery/web/security/SecurityConfig.java
+++ src/main/java/com/clothesdelivery/web/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
             "/login", "/signup",
             "/terms", "/", "/about", "/contact", "/error", "/notfound",
             "/css/**",  "/fonts/**", "/js/**", "/images/**", "/libs/**",
-            "/products", "/detail/**",
+            "/products", "/detail/**", "/admin/**"
     };
 
     private final CustomUserDetailsService _userDetailsService;
