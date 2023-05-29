42195a058e60f51abba446bd5a4e2c2cddd60275
Joaquim José <joaquimmatoso01@gmail.com>
2023-05-29 10:16:27 +0100

    Dashboard p1

--- src/main/java/com/clothesdelivery/web/controllers/AdminController.java
+++ src/main/java/com/clothesdelivery/web/controllers/AdminController.java
@@ -1,6 +1,9 @@
 package com.clothesdelivery.web.controllers;
 
+import com.clothesdelivery.web.enums.OrderStatus;
 import com.clothesdelivery.web.enums.Role;
+import com.clothesdelivery.web.repositories.IOrderItemRepository;
+import com.clothesdelivery.web.repositories.IOrderRepository;
 import com.clothesdelivery.web.repositories.IProductRepository;
 import com.clothesdelivery.web.repositories.IUserRepository;
 import org.jetbrains.annotations.NotNull;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+
 @Controller
 @RequestMapping("/admin")
 public class AdminController extends BaseController {
@@ -20,8 +25,23 @@ public class AdminController extends BaseController {
     @Autowired
     private IUserRepository _users;
 
+    @Autowired
+    private IOrderRepository _orders;
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(@NotNull Model model) {
+        var customers = _users.findAllByRole(Role.ROLE_CUSTOMER).size();
+        var products = _products.count();
+
+        var orders = _orders.findAll();
+        var monthlyOrders = orders.stream().filter(e -> e.getCreatedTime().getMonth() == LocalDate.now().getMonth()).count();
+        var pendingOrders = orders.stream().filter(e -> e.getStatus() == OrderStatus.Pendent).count();
+
+        model.addAttribute("customers", customers);
+        model.addAttribute("products", products);
+        model.addAttribute("monthly_orders", monthlyOrders);
+        model.addAttribute("pending_orders", pendingOrders);
+
         return "admin/dashboard";
     }
 
--- src/main/java/com/clothesdelivery/web/repositories/IUserRepository.java
+++ src/main/java/com/clothesdelivery/web/repositories/IUserRepository.java
@@ -1,10 +1,14 @@
 package com.clothesdelivery.web.repositories;
 
 import com.clothesdelivery.web.entities.User;
+import com.clothesdelivery.web.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    List<User> findAllByRole(Role role);
     User findByEmailAndPassword(String email, String password);
     boolean existsByEmailOrPhoneNumber(String email, String phoneNumber);
 
